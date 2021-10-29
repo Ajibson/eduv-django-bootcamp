36ad9bef03c271a4e62dfc44f6ec11822a28113d
Ajibson <azeezajibson123@gmail.com>
2021-10-29 18:25:19 +0100

    heroku confugurations

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
